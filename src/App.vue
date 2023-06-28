701b69a5cdd8b9faa925f62d38708f4360843bf8
Yana <norinskaya.yanaa@yandex.ru>
2023-06-28 19:22:26 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TextInput from "@/components/UI/TextInput/TextInput.vue";
+import { ref } from "vue";
+const title = ref("");
+function setValue(val: string) {
+	title.value = val;
+}
+</script>
+<template>
+	<span>{{ title }}</span>
+	<TextInput
+		:model-value="title"
+		@update:model-value="setValue"
+	/>
+</template>
---
+++ src/components/UI/TextInput/TextInput.vue
@@ -0,0 +1,56 @@
+<template>
+	<input
+		v-model="value"
+		type="text"
+		:class="[$style.input, error ? $style.error : '']"
+		@input="handleInput"
+	>
+	<span
+		v-if="error"
+		class="error-message"
+	>{{ error }}</span>
+</template>
+  
+<script setup lang="ts">
+import { ref } from "vue";
+import { useCssModule } from "vue";
+interface TextInputProps {
+	modelValue: string;
+	error?: string;
+}
+const $style = useCssModule();
+  
+const props: TextInputProps = defineProps({
+	modelValue: {
+		type: String,
+		required: true,
+	},
+	error: {
+		type: String,
+		default: "",
+	},
+});
+const propsValue = ref(props.modelValue);
+const emit = defineEmits(["update:modelValue"]);
+const value = ref(props.modelValue);
+const handleInput = (event: Event) => {
+	propsValue.value = (event.target as HTMLInputElement).value;
+	emit("update:modelValue", value.value);
+};
+</script>
+  
+  <style module lang="scss">
+  .input {
+    width: 100%;
+    border-radius: 5px;
+    padding: 6px;
+    border: 2px solid var(--border-secondary-color);
+  }
+  .error {
+    border-color: red;
+  }
+  .error-message {
+    color: red;
+  }
+  </style>
+  
